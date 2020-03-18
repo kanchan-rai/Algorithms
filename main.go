468bf20eb30a1c2fc718620a88968f6f0c3ae12a
kanchan-rai <62348649+kanchan-rai@users.noreply.github.com>
2020-03-19 01:42:27 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+// endpoints.go
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// HealthCheckHandler : Healthcheck
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/health", HealthCheckHandler)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
